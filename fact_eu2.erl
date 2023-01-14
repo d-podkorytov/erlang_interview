d7a65ba3cb928e0cc1f6350f0a5ebc44fbe9b8c4
Dmitrii Podkorytov <podkorytov@mail.ru>
2023-01-14 13:29:46 -0600

    Add files via upload

--- fact_eu2.erl
+++ fact_eu2.erl
@@ -1,9 +1,9 @@
 -module(fact_eu2).
--export([fact/1]).
+-export([fact/1,fact_l/1]).
 
 -ifdef(TEST).
- -include_lib("eunit/include/eunit.hrl").
- -compile(export_all).
+-include_lib("eunit/include/eunit.hrl").
+-compile(export_all).
 -endif.
 
 -define(ERROR_INFO(ERROR_TYPE,X), {error,ERROR_TYPE,[N]}).
@@ -18,8 +18,44 @@ fact(0) -> 1;
 fact(1) -> 1;
 fact(N) when is_integer(N) andalso N>=1  -> N*fact(N-1);
 fact(N) when N <0 andalso is_integer(N)  -> ?ERROR_INFO(<<"negative input">>,N);
-
 fact(N) -> ?ERROR_INFO(<<"bad input">>,N).
+
+loop()->
+ receive
+  {FromPid,N}  -> FromPid!{FromPid,f(N)}  
+ end, 
+ loop().
+
+fact(0,Acc) -> Acc;
+fact(1,Acc) -> Acc;
+fact(N,Acc) -> fact((N-1),N*Acc).
+
+f(N)->fact(N,1).
+
+c(Pid,N)-> 
+ Pid!{self(),N},
+ receive
+  {FromPid,Res}  -> Res  
+ after 1000-> timeout
+ end.
+
+c(Pid,N,ReplyTo)-> Pid!{ReplyTo,N}.
+
+
+cv(PidL,N) -> L=lists:seq(N),
+ lists:foldl(fun(A,Acc)->
+            CpuNo = Acc rem 4,
+             spawn(fun() -> c(lists:nth(CpuNo,PidL),A,self()) end),
+            Acc+1
+           end, L). 
+
+fact_l(N) when is_float(N) ->?ERROR_INFO(<<"float in input">>,N);
+fact_l(N) when N <0 andalso is_integer(N)  -> ?ERROR_INFO(<<"negative input">>,N);
+fact_l(N) when not is_integer(N)  -> ?ERROR_INFO(<<"bad input">>,N);
+
+fact_l(0) -> 1;
+fact_l(N) -> L=lists:seq(1,N),
+             lists:foldl(fun(A,Acc)-> A*Acc end,1, L).
 
 % ======= TESTS SECTION =================
 
@@ -67,7 +103,19 @@ fact_test_() ->
 	?_assert(fact([])  =:= {error, <<"bad input">>     ,[[]] }),
         ?_assert(fact(1.1) =:= {error, <<"float in input">>,[1.1]}), 
 	?_assert(fact(10)  =:= 3628800),
-        ?_assert(fact(20)  =:= 2432902008176640000) 
+        ?_assert(fact(20)  =:= 2432902008176640000),
+ 
+        ?_assert(fact_l(0)   =:= 1),
+	?_assert(fact_l(1)   =:= 1),
+	?_assert(fact_l(2)   =:= 2),
+	?_assert(fact_l(3)   =:= 6),
+	?_assert(fact_l(4)   =:= 24),
+	?_assert(fact_l(-1)  =:= {error, <<"negative input">>,[-1] }),
+	?_assert(fact_l([])  =:= {error, <<"bad input">>     ,[[]] }),
+        ?_assert(fact_l(1.1) =:= {error, <<"float in input">>,[1.1]}), 
+	?_assert(fact_l(10)  =:= 3628800),
+        ?_assert(fact_l(20)  =:= 2432902008176640000) 
+       
        ].
 -endif.
 % ========= END TESTS ===================
